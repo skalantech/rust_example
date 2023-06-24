a521497e60dbb574156844b1937daa083526b761
skalantech <escacarlos1982@gmail.com>
2023-06-24 13:02:13 -0400

    from and into traits

--- src/re6.rs
+++ src/re6.rs
@@ -1,3 +1,23 @@
+#![allow(unused)]
 pub fn example() {
-    
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+    println!("{my_string}");
+    println!("{my_str}");
+
+    let num = Number::from(30);
+    println!("The number is {:?}", num);
+}
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item}
+    }
 }
